b44bfbe8cc66ba5888b2e74e275305348aa33fc3
Sandra Kahl <sandra.kahl@hrz.tu-chemnitz.de>
2021-10-23 21:33:22 +0200

    Adjustments Responsive CSS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $('.modal').on('shown.bs.modal', function () {
+    $('.modal.sa-video').on('shown.bs.modal', function () {
         var video_id = "#" + $(this).attr("id") + "_video";
         $(video_id)[0].play();
     });
-    $('.modal').on('hidden.bs.modal', function () {
+    $('.modal.sa-video').on('hidden.bs.modal', function () {
         var video_id = "#" + $(this).attr("id") + "_video";
         $(video_id)[0].pause();
     });
