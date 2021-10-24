cd14b341a2b38911cdf0d8f2e163e4ef6ed453e5
Sandra Kahl <sandra.kahl@hrz.tu-chemnitz.de>
2021-10-24 20:56:45 +0200

    Navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // autoplay on video modals
     $('.modal.sa-video').on('shown.bs.modal', function () {
         var video_id = "#" + $(this).attr("id") + "_video";
         $(video_id)[0].play();
@@ -6,5 +7,11 @@ $(document).ready(function(){
     $('.modal.sa-video').on('hidden.bs.modal', function () {
         var video_id = "#" + $(this).attr("id") + "_video";
         $(video_id)[0].pause();
+    });
+
+    // navbar scroll functions
+    $(window).scroll(function(e) {
+        // add/remove class to navbar when scrolling to hide/show
+        $('.main-navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
     });
 });
